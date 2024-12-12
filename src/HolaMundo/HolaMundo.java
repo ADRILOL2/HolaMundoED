1c88d1c2516e248f6f944b62640fcd39359bb584
ADRILOL2 <aavila20479@iesjoanramis.org>
2024-12-12 11:07:40 +0100

    Programa con comentarios Javadoc

--- src/HolaMundo/HolaMundo.java
+++ src/HolaMundo/HolaMundo.java
@@ -1,16 +1,25 @@
 package HolaMundo;
 import java.util.Scanner;
 public class HolaMundo {
-
+	/* Lo que hace el programa es pedirte de ingresar tu nombre,
+	 * Cuando ya añadiste tu nombre, se guarda en la variable "nombre"
+	 * Para luego expulsar el comando dandote la bienvenida con el mismo nombre que has puesto
+	 * 
+	 * @autor Adrian Avila
+	 * @version 1.2
+	 */
+	
+	
 	public static void main(String[] args) {
-		//Cambio realizado desde rama Desarrollo
-		//Comentario después de realizar fusión
-
 		Scanner scanner = new Scanner(System.in);
+		// Pide tu nombre
 		System.out.print("Ingresa tu nombre: ");
+		
+		//envio y guardado de texto a variable nombre
 		String nombre = scanner.nextLine();
+		
+		//Ejecuta la bienvenida con el nombre añadido anteriormente
 		System.out.println("¡Hola, " + nombre + "! Bienvenido a Java.");
-		//Este cambio se realiza desde github
-		//Cambio realizado despues de hacerl el Pull
+		
 	}
 }
